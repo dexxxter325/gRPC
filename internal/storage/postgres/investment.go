0492e7d0e7797f45c985c43e5a4d3a2b1f20e209
dexxxter325 <prsokol2005@gmail.com>
2024-02-27 01:19:44 +0300

    updated repo

--- internal/storage/postgres/investment.go
+++ internal/storage/postgres/investment.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"GRPC/internal/domain/models"
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -15,13 +16,39 @@ func NewInvestmentPostgres(db *pgxpool.Pool) *InvestmentPostgres {
 }
 
 func (p *InvestmentPostgres) Create(ctx context.Context, amount int64, currency string) (investmentId int64, err error) {
-	panic("implement me ")
+	query := `insert into investments(amount,currency) values $1,$2 returning id`
+	row := p.db.QueryRow(ctx, query, amount, currency)
+	if err = row.Scan(&investmentId); err != nil {
+		return 0, fmt.Errorf("scan failed in create:%s", err)
+	}
+	return investmentId, err
 }
 
-func (p *InvestmentPostgres) Get(ctx context.Context) (investment models.Investment, err error) {
-	panic("implement me ")
+func (p *InvestmentPostgres) Get(ctx context.Context) (investment []models.Investment, err error) {
+	query := `select * from investments ORDER BY id ASC`
+	row, err := p.db.Query(ctx, query)
+	if err != nil {
+		return []models.Investment{}, fmt.Errorf("query failed in get:%s", err)
+	}
+	defer row.Close()
+	for row.Next() { //читает каждую строку из результата sql query ,чтобы затем добавить в срез
+		var investments models.Investment
+		if err = row.Scan(&investments.ID, &investments.Amount, &investments.Currency); err != nil {
+			return []models.Investment{}, fmt.Errorf("scan failed in get:%s", err)
+		}
+		investment = append(investment, investments) //с каждой успешно прочитанной строкой,добавляем эл-т в срез
+	}
+	return investment, nil
 }
 
 func (p *InvestmentPostgres) Delete(ctx context.Context, investmentId int64) error {
-	panic("implement me ")
+	query := `delete from investments where id=$1`
+	row, err := p.db.Exec(ctx, query, investmentId)
+	if err != nil {
+		return fmt.Errorf("exec failed in delete:%s", err)
+	}
+	if row.RowsAffected() == 0 {
+		return fmt.Errorf("already deleted or doesn't exist:%s", err)
+	}
+	return nil
 }
--- internal/storage/postgres/user.go
+++ internal/storage/postgres/user.go
@@ -3,6 +3,9 @@ package postgres
 import (
 	"GRPC/internal/domain/models"
 	"context"
+	"errors"
+	"fmt"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -15,9 +18,24 @@ func NewUserPostgres(db *pgxpool.Pool) *UserPostgres {
 }
 
 func (p *UserPostgres) SaveUser(ctx context.Context, email string, hashedPassword []byte) (userId int64, err error) {
-	panic("implement me !")
+	query := `insert into users(email,password) values $1,$2,$3 returning id`
+	row := p.db.QueryRow(ctx, query, email, hashedPassword)
+	if err = row.Scan(&userId); err != nil {
+		return 0, fmt.Errorf("scan failed in SaveUser:%s", err)
+	}
+	return userId, nil
+
 }
 
 func (p *UserPostgres) GetUserByEmail(ctx context.Context, email string) (user models.User, err error) {
-	panic("implement me!")
+	query := `select * from users where email=$1`
+	row := p.db.QueryRow(ctx, query, email)
+	err = row.Scan(&user.ID, &user.Email, &user.Password)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return models.User{}, fmt.Errorf("user not found with email: %s", email)
+		}
+		return models.User{}, fmt.Errorf("scan failed in getuser:%s", err)
+	}
+	return user, nil
 }
